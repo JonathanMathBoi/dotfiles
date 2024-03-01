efa16b40199f6a30bde6cf6140161d9abe4236b3
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-03-01 14:31:29 -0500

    Added git commit rulers
    Added color columns at 50 and 72 chars when writing a git commit message

--- .config/nvim/lua/jhart/init.lua
+++ .config/nvim/lua/jhart/init.lua
@@ -2,3 +2,12 @@ require("jhart.remap")
 require("jhart.lazy")
 require("jhart.catppuccin")
 
+local function git_commit_rulers()
+    vim.opt.cc = "50,72"
+end
+
+vim.api.nvim_create_autocmd({"FileType"}, {
+    pattern = {"gitcommit"},
+    callback = git_commit_rulers
+})
+
