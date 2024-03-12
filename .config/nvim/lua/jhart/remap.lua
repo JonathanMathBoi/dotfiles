b488aa8cca53c4b76696ad638906030871ba6f39
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-03-12 13:25:02 -0400

    Added control keybinds for moving in Nvim

--- .config/nvim/lua/jhart/remap.lua
+++ .config/nvim/lua/jhart/remap.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Set <C-direction> to move between splits in normal and terminal mode
+vim.keymap.set({'n', 't'}, '<C-h>', '<C-w>h', {})
+vim.keymap.set({'n', 't'}, '<C-j>', '<C-w>j', {})
+vim.keymap.set({'n', 't'}, '<C-k>', '<C-w>k', {})
+vim.keymap.set({'n', 't'}, '<C-l>', '<C-w>l', {})
+
