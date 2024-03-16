8e12a8297957461f25e150357dce3806f57b3a31
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-03-15 22:16:42 -0400

    Added autocommand to format files on save

--- .config/nvim/lua/jhart/init.lua
+++ .config/nvim/lua/jhart/init.lua
@@ -6,8 +6,8 @@ local function git_commit_rulers()
     vim.opt.cc = "50,72"
 end
 
-vim.api.nvim_create_autocmd({"FileType"}, {
-    pattern = {"gitcommit"},
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = { "gitcommit" },
     callback = git_commit_rulers
 })
 
@@ -15,8 +15,14 @@ local function vimwiki_ruler()
     vim.opt.cc = "100"
 end
 
-vim.api.nvim_create_autocmd({'FileType'}, {
-    pattern = {'vimwiki'},
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+    pattern = { 'vimwiki' },
     callback = vimwiki_ruler
 })
 
+-- Use LSP to format file before saving
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+    callback = function()
+        vim.lsp.buf.format()
+    end
+})
