feec99f233fe1aaebe5db98faf100d6f89b43540
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-03-25 11:59:34 -0400

    Added tab keybindings to neovim

--- .config/nvim/lua/jhart/remap.lua
+++ .config/nvim/lua/jhart/remap.lua
@@ -2,8 +2,11 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Set <C-direction> to move between splits in normal and terminal mode
-vim.keymap.set({'n', 't'}, '<C-h>', '<C-w>h', {})
-vim.keymap.set({'n', 't'}, '<C-j>', '<C-w>j', {})
-vim.keymap.set({'n', 't'}, '<C-k>', '<C-w>k', {})
-vim.keymap.set({'n', 't'}, '<C-l>', '<C-w>l', {})
+vim.keymap.set({ 'n', 't' }, '<C-h>', '<C-w>h', {})
+vim.keymap.set({ 'n', 't' }, '<C-j>', '<C-w>j', {})
+vim.keymap.set({ 'n', 't' }, '<C-k>', '<C-w>k', {})
+vim.keymap.set({ 'n', 't' }, '<C-l>', '<C-w>l', {})
 
+-- Set next and previous for tabs
+vim.keymap.set('n', '<leader>tn', vim.cmd.tabnext)
+vim.keymap.set('n', '<leader>tp', vim.cmd.tabprev)
