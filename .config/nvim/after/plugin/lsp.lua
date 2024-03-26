75a332e598505a1b8b2313ad518b559315d92ee1
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-03-26 10:32:12 -0400

    Added clangd configuration to neovim

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,12 +1,21 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  handlers = {
-    lsp_zero.default_setup,
-  },
+    handlers = {
+        lsp_zero.default_setup,
+    },
+})
+
+local lspconfig = require('lspconfig')
+
+lspconfig.clangd.setup({
+    cmd = {
+        "clangd",
+        "--function-arg-placeholders=0"
+    }
 })
