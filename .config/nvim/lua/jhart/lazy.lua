5ffb317c1a945a02ce6bebf6a3af689e0f3ae012
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-03-27 19:26:36 -0400

    Updated lazy.lua formatting

--- .config/nvim/lua/jhart/lazy.lua
+++ .config/nvim/lua/jhart/lazy.lua
@@ -1,71 +1,75 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.4',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
-  {'williamboman/mason.nvim'},
-  {'williamboman/mason-lspconfig.nvim'},
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-  {'neovim/nvim-lspconfig'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/nvim-cmp'},
-  {'L3MON4D3/LuaSnip'},
-  {'ThePrimeagen/vim-be-good'},
-  {'lewis6991/gitsigns.nvim'},
-  {
-      'nvim-lualine/lualine.nvim',
-      dependencies = { 'nvim-tree/nvim-web-devicons' }
-  },
-  {
-      'windwp/nvim-autopairs',
-      event = "InsertEnter",
-      config = true
-  },
-  {'tpope/vim-fugitive'},
-  {
-      'stevearc/aerial.nvim',
-      opts = {},
-      dependencies = {
-         "nvim-treesitter/nvim-treesitter",
-         "nvim-tree/nvim-web-devicons"
-      },
-  },
-  {
-      'vimwiki/vimwiki',
-      init = function()
-          vim.g.vimwiki_list = {
-              {
-                  path = '~/documents/vimwiki/',
-                  syntax = 'markdown',
-                  ext = '.md'
-              }
-          }
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.4',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { 'catppuccin/nvim',                  name = 'catppuccin', priority = 1000 },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+    { "nvim-treesitter/nvim-treesitter",  build = ":TSUpdate" },
+    { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'L3MON4D3/LuaSnip' },
+    { 'ThePrimeagen/vim-be-good' },
+    { 'lewis6991/gitsigns.nvim' },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+    },
+    { 'tpope/vim-fugitive' },
+    {
+        'stevearc/aerial.nvim',
+        opts = {},
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons"
+        },
+    },
+    {
+        'vimwiki/vimwiki',
+        init = function()
+            vim.g.vimwiki_list = {
+                {
+                    path = '~/documents/vimwiki/',
+                    syntax = 'markdown',
+                    ext = '.md'
+                }
+            }
 
-          vim.g.vimwiki_global_ext = 0
-      end
-  },
-  {'mbbill/undotree'},
-  {
+            vim.g.vimwiki_global_ext = 0
+        end
+    },
+    { 'mbbill/undotree' },
+    {
         'pwntester/octo.nvim',
         dependencies = {
             'nvim-lua/plenary.nvim',
             'nvim-telescope/telescope.nvim',
             'nvim-tree/nvim-web-devicons'
-        }
-  }
+        },
+        config = function()
+            require("octo").setup()
+        end
+    }
 })
