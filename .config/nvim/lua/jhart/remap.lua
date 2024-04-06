dfbb9d52c144424e8a0d2e5d9dba42dff0fc9b59
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-04-06 19:30:32 -0400

    Added <leader>q to quit file in Neovim

--- .config/nvim/lua/jhart/remap.lua
+++ .config/nvim/lua/jhart/remap.lua
@@ -10,3 +10,6 @@ vim.keymap.set({ 'n', 't' }, '<C-l>', '<C-w>l', {})
 -- Set next and previous for tabs
 vim.keymap.set('n', '<leader>tn', vim.cmd.tabnext)
 vim.keymap.set('n', '<leader>tp', vim.cmd.tabprev)
+
+-- Set <leader>q to quit the current file
+vim.keymap.set('n', '<leader>q', vim.cmd.quit)
