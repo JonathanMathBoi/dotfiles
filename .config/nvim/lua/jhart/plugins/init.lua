81f612a9b2719a3e3053e617f5c24a92195d09bf
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-05-04 09:07:21 -0400

    Moved nvim plugins to plugins/init.lua
    First stage of converting to lazy.lua configuration idioms.

---
+++ .config/nvim/lua/jhart/plugins/init.lua
@@ -0,0 +1,71 @@
+return {
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.4',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { 'catppuccin/nvim',                  name = 'catppuccin', priority = 1000 },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+    { "nvim-treesitter/nvim-treesitter",  build = ":TSUpdate" },
+    { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'L3MON4D3/LuaSnip' },
+    { 'ThePrimeagen/vim-be-good' },
+    { 'lewis6991/gitsigns.nvim' },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+    },
+    { 'tpope/vim-fugitive' },
+    {
+        'stevearc/aerial.nvim',
+        opts = {},
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons"
+        },
+    },
+    {
+        'vimwiki/vimwiki',
+        init = function()
+            vim.g.vimwiki_list = {
+                {
+                    path = '~/documents/vimwiki/',
+                    syntax = 'markdown',
+                    ext = '.md'
+                }
+            }
+
+            vim.g.vimwiki_global_ext = 0
+        end
+    },
+    { 'mbbill/undotree' },
+    {
+        'pwntester/octo.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim',
+            'nvim-tree/nvim-web-devicons'
+        },
+        config = function()
+            require("octo").setup()
+        end
+    },
+    { 'HiPhish/rainbow-delimiters.nvim' },
+    {
+        'stevearc/oil.nvim',
+        opts = {},
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require("oil").setup()
+        end,
+    }
+}
