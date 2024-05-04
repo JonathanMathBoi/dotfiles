1dc93d838264a48dedc77f2777cd914a56cb4b9b
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-05-04 09:38:36 -0400

    Moved vimwiki config to its own file

--- .config/nvim/lua/jhart/plugins/init.lua
+++ .config/nvim/lua/jhart/plugins/init.lua
@@ -30,20 +30,6 @@ return {
             "nvim-tree/nvim-web-devicons"
         },
     },
-    {
-        'vimwiki/vimwiki',
-        init = function()
-            vim.g.vimwiki_list = {
-                {
-                    path = '~/documents/vimwiki/',
-                    syntax = 'markdown',
-                    ext = '.md'
-                }
-            }
-
-            vim.g.vimwiki_global_ext = 0
-        end
-    },
     { 'mbbill/undotree' },
     {
         'pwntester/octo.nvim',
