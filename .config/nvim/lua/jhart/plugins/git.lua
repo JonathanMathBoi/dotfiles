77fd7f175562a410885d19cd5046ab2fd77b491e
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-06-18 09:34:10 -0400

    Updated nvim plugins
    Updated Neovim Plugins
    Reconfigured git.lua to use opts field

--- .config/nvim/lua/jhart/plugins/git.lua
+++ .config/nvim/lua/jhart/plugins/git.lua
@@ -1,28 +1,26 @@
 return {
     {
         'lewis6991/gitsigns.nvim',
-        config = function()
-            require('gitsigns').setup({
-                on_attach = function(bufnr)
-                    local gs = package.loaded.gitsigns
+        opts = {
+            on_attach = function(bufnr)
+                local gs = package.loaded.gitsigns
 
-                    local function map(mode, l, r, opts)
-                        opts = opts or {}
-                        opts.buffer = bufnr
-                        vim.keymap.set(mode, l, r, opts)
-                    end
-
-                    -- Keybinds for hunk staging
-                    map('n', '<leader>hs', gs.stage_hunk)
-                    map('n', '<leader>hr', gs.reset_hunk)
-                    map('n', '<leader>hu', gs.undo_stage_hunk)
-
-                    -- Keybinds for buffer staging
-                    map('n', '<leader>hS', gs.stage_buffer)
-                    map('n', '<leader>hR', gs.reset_buffer)
+                local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
                 end
-            })
-        end
+
+                -- Keybinds for hunk staging
+                map('n', '<leader>hs', gs.stage_hunk)
+                map('n', '<leader>hr', gs.reset_hunk)
+                map('n', '<leader>hu', gs.undo_stage_hunk)
+
+                -- Keybinds for buffer staging
+                map('n', '<leader>hS', gs.stage_buffer)
+                map('n', '<leader>hR', gs.reset_buffer)
+            end
+        },
     },
     {
         'tpope/vim-fugitive',
