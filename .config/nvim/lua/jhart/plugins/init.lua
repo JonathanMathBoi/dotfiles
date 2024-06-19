c67cd41386fdd083a514c6ffc5e1557698e4b2d3
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-06-19 18:18:05 -0400

    Moved octo.nvim config to its own file

--- .config/nvim/lua/jhart/plugins/init.lua
+++ .config/nvim/lua/jhart/plugins/init.lua
@@ -6,18 +6,5 @@ return {
         config = true
     },
     { 'mbbill/undotree' },
-    {
-        'pwntester/octo.nvim',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            'nvim-telescope/telescope.nvim',
-            'nvim-tree/nvim-web-devicons'
-        },
-        opts = {
-            suppress_missing_scope = {
-                projects_v2 = true,
-            },
-        },
-    },
     { 'HiPhish/rainbow-delimiters.nvim' },
 }
