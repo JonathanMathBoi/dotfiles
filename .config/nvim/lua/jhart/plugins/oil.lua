0da01865cf79afdaa303f4ab71cf674a96a34c20
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-06-19 10:29:11 -0400

    Updated oil.nvim config to lazy style

--- .config/nvim/lua/jhart/plugins/oil.lua
+++ .config/nvim/lua/jhart/plugins/oil.lua
@@ -2,8 +2,9 @@ return {
     'stevearc/oil.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {},
-    config = function(_, opts)
-        require('oil').setup(opts)
-        vim.keymap.set('n', '-', vim.cmd.Oil)
-    end,
+    cmd = 'Oil',
+    event = { 'VimEnter */*,.*', 'BufNew */*,.*', },
+    keys = {
+        { '-', vim.cmd.Oil, desc = 'Oil Parent Dir', },
+    },
 }
