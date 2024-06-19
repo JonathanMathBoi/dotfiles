fbc5f049c4cee8a32309df0cd6d5ec3091a96459
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-06-19 17:06:04 -0400

    Cleaned lsp-zero config

--- .config/nvim/lua/jhart/plugins/lsp.lua
+++ .config/nvim/lua/jhart/plugins/lsp.lua
@@ -1,60 +1,57 @@
 return {
-    -- LSP Zero
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        dependencies = {
-            -- LSP Management
-            'neovim/nvim-lspconfig',
-            'williamboman/mason.nvim',
-            'williamboman/mason-lspconfig.nvim',
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    dependencies = {
+        -- LSP Management
+        'neovim/nvim-lspconfig',
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
 
-            -- Autocompletion Plugins
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/nvim-cmp',
-            'L3MON4D3/LuaSnip',
-        },
-        config = function()
-            local lsp_zero = require('lsp-zero')
+        -- Autocompletion Plugins
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/nvim-cmp',
+        'L3MON4D3/LuaSnip',
+    },
+    config = function()
+        local lsp_zero = require('lsp-zero')
 
-            lsp_zero.on_attach(function(client, bufnr)
-                -- see :help lsp-zero-keybindings
-                -- to learn the available actions
-                lsp_zero.default_keymaps({ buffer = bufnr })
-            end)
+        lsp_zero.on_attach(function(client, bufnr)
+            -- see :help lsp-zero-keybindings
+            -- to learn the available actions
+            lsp_zero.default_keymaps({ buffer = bufnr })
+        end)
 
-            -- to learn how to use mason.nvim
-            -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-            require('mason').setup({})
-            require('mason-lspconfig').setup({
-                ensure_installed = { 'lua_ls', 'clangd' },
-                handlers = {
-                    -- this function is the "default handler"
-                    -- it applies to every language server without a "custom handler"
-                    function(server_name)
-                        require('lspconfig')[server_name].setup({})
-                    end,
-                },
-            })
+        -- to learn how to use mason.nvim
+        -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+        require('mason').setup({})
+        require('mason-lspconfig').setup({
+            ensure_installed = { 'lua_ls', 'clangd' },
+            handlers = {
+                -- this function is the "default handler"
+                -- it applies to every language server without a "custom handler"
+                function(server_name)
+                    require('lspconfig')[server_name].setup({})
+                end,
+            },
+        })
 
-            local lspconfig = require('lspconfig')
+        local lspconfig = require('lspconfig')
 
-            lspconfig.clangd.setup({
-                cmd = {
-                    "clangd",
-                    "--function-arg-placeholders=0"
-                }
-            })
+        lspconfig.clangd.setup({
+            cmd = {
+                "clangd",
+                "--function-arg-placeholders=0"
+            }
+        })
 
-            lspconfig.lua_ls.setup({
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { 'vim' }
-                        }
+        lspconfig.lua_ls.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { 'vim' }
                     }
                 }
-            })
-        end,
-    },
+            }
+        })
+    end,
 }
