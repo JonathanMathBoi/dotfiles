40de3ec592fd20deab3dc93ad457bd2083073693
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-06-19 16:38:35 -0400

    Various Neovim improvements
    Added which-key.nvim
    Consolidated LSP config
    Updated Telescope config to lazy style

--- .config/nvim/lua/jhart/plugins/telescope.lua
+++ .config/nvim/lua/jhart/plugins/telescope.lua
@@ -2,11 +2,10 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    end
+    keys = {
+        { '<leader>ff', require('telescope.builtin').find_files, desc = 'Find Files', },
+        { '<leader>fg', require('telescope.builtin').live_grep,  desc = 'Live Grep', },
+        { '<leader>fb', require('telescope.builtin').buffers,    desc = 'Show Buffers', },
+        { '<leader>fh', require('telescope.builtin').help_tags,  desc = 'Neovim Docs', },
+    },
 }
