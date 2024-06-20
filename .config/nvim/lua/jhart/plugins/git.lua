a0d8a86ca0ada34bf06028ebf4ecc8a4df2f4036
Jonathan Hart <jonathan.e.hart@outlook.com>
2024-06-20 16:02:14 -0400

    Added descriptions to gitsigns keymaps

--- .config/nvim/lua/jhart/plugins/git.lua
+++ .config/nvim/lua/jhart/plugins/git.lua
@@ -12,13 +12,13 @@ return {
                 end
 
                 -- Keybinds for hunk staging
-                map('n', '<leader>hs', gs.stage_hunk)
-                map('n', '<leader>hr', gs.reset_hunk)
-                map('n', '<leader>hu', gs.undo_stage_hunk)
+                map('n', '<leader>hs', gs.stage_hunk, { desc = "Stage Hunk" })
+                map('n', '<leader>hr', gs.reset_hunk, { desc = "Reset Hunk" })
+                map('n', '<leader>hu', gs.undo_stage_hunk, { desc = "Undo Stage Hunk" })
 
                 -- Keybinds for buffer staging
-                map('n', '<leader>hS', gs.stage_buffer)
-                map('n', '<leader>hR', gs.reset_buffer)
+                map('n', '<leader>hS', gs.stage_buffer, { desc = "Stage Buffer" })
+                map('n', '<leader>hR', gs.reset_buffer, { desc = "Reset Buffer" })
             end
         },
     },
