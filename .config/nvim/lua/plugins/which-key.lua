4a1364f4477ac0ceee79b4c1d2de527d38a5c640
Jonathan Hart <jonathan.e.hart@outlook.com>
2025-12-18 17:44:00 -0500

    Added which-key to neovim

---
+++ .config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,28 @@
+return {
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  opts = {},
+  keys = {
+    {
+      '<leader>?',
+      function()
+        require('which-key').show({ global = false })
+      end,
+      desc = 'Buffer Local Keymaps (which-key)',
+    },
+  },
+  config = function(_, opts)
+    local wk = require('which-key')
+    wk.setup(opts)
+
+    wk.add({
+      { '<leader>g', group = 'Git' },
+      { '<leader>h', group = 'Git Hunks' },
+      { '<leader>b', group = 'Buffer' },
+    })
+  end,
+}
