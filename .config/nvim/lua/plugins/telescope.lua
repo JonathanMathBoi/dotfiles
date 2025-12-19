025ba2f7b136421fb0f264d0ab17f862de73d41d
Jonathan Hart <jonathan.e.hart@outlook.com>
2025-12-18 19:40:51 -0500

    Added Telescope to nvim

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,15 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = 'v0.2.0',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {},
+  config = function(_, opts)
+    require('telescope').setup(opts)
+
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find Files' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live Grep' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Show Buffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Neovim Docs' })
+  end,
+}
