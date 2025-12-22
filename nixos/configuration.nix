99034919793a8ee165350d6fae0a9046f977eb40
Jonathan Hart <jonathan.e.hart@outlook.com>
2025-12-21 19:06:42 -0500

    Added more programs to NixOS config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,6 +10,8 @@
       ./hardware-configuration.nix
     ];
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -37,11 +39,7 @@
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-
-  
+  programs.hyprland.enable = true;
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
@@ -61,14 +59,8 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     tree
-  #   ];
-  # };
+  programs.fish.enable = true;
+
   users.users.jonathan = {
       isNormalUser = true;
       extraGroups = [ "wheel" "networkmanager" "video" "audio" ];
@@ -76,12 +68,30 @@
 
   # programs.firefox.enable = true;
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  environment.systemPackages = with pkgs; [
+    # Browser
+    brave
+
+    # Terminal & Editor
+    alacritty
+    neovim
+  
+    # Desktop & Utilities
+    waybar
+    hyprpaper
+    hyprlock
+    hypridle
+    dunst
+    wofi
+    git
+  
+    # Tools for the terminal
+    fish
+    starship
+    zoxide
+    yazi
+    bat
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
