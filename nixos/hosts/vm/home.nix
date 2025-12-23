5288571fb373bb709ec4e1af8244cdc658a519a8
Jonathan Hart <jonathan.e.hart@outlook.com>
2025-12-23 16:16:55 -0500

    Moved user dir creation to seprate nix module

--- nixos/hosts/vm/home.nix
+++ nixos/hosts/vm/home.nix
@@ -6,23 +6,10 @@
 
   imports = [
     ../../modules/home/common.nix
+    ../../modules/home/user-dirs.nix
   ];
 
   programs.git.signing.key = "8D216E5BA2708CD8";
-
-  xdg.userDirs = {
-    enable = true;
-    createDirectories = true;
-
-    desktop = "${config.home.homeDirectory}/desktop";
-    documents = "${config.home.homeDirectory}/documents";
-    download = "${config.home.homeDirectory}/downloads";
-    music = "${config.home.homeDirectory}/music";
-    pictures = "${config.home.homeDirectory}/pictures";
-    videos = "${config.home.homeDirectory}/videos";
-    publicShare = "${config.home.homeDirectory}/public";
-    templates = "${config.home.homeDirectory}/templates";
-  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
