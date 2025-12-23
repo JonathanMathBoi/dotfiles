26cd2dea005504a4c6d7c33b5546920512fe25d6
Jonathan Hart <jonathan.e.hart@outlook.com>
2025-12-23 14:49:12 -0500

    Modularized bat and yazi nix config

--- nixos/hosts/vm/home.nix
+++ nixos/hosts/vm/home.nix
@@ -28,17 +28,6 @@
     ../../modules/home/common.nix
   ];
 
-  programs.bat = {
-    enable = true;
-    # TODO: Theme with catppuccin
-  };
-
-  programs.yazi = {
-    enable = true;
-    shellWrapperName = "ya";
-    enableFishIntegration = true;
-  };
-
   programs.git = {
     enable = true;
 
@@ -87,12 +76,6 @@
       # Renumber windows when one closes
       set -g renumber-windows on
     '';
-  };
-
-  home.sessionVariables = {
-    # Use bat for coloring man pages
-    MANPAGER = "sh -c 'col -bx | bat -l man -p'";
-    MANROFFOPT = "-c";
   };
 
   xdg.configFile."nvim".source =
---
+++ nixos/modules/home/terminal-tools.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+{
+  programs.bat = {
+    enable = true;
+    # TODO: Theme with catppuccin
+  };
+
+  programs.yazi = {
+    enable = true;
+    shellWrapperName = "ya";
+    enableFishIntegration = true;
+  };
+
+  home.sessionVariables = {
+    # Use bat for coloring man pages
+    MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+    MANROFFOPT = "-c";
+  };
+}
