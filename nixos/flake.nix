acce99b45756e690b4aaf82d45fae30f84733d34
Jonathan Hart <jonathan.e.hart@outlook.com>
2025-12-23 10:50:29 -0500

    Moved home.nix into hosts/vm/

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -21,7 +21,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.jonathan = import ./home.nix;
+            home-manager.users.jonathan = import ./hosts/vm/home.nix;
           }
         ];
       };
