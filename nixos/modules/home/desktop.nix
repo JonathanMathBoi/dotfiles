3ba17e5aa2d26f96e692504f1c55284b01acb92e
Jonathan Hart <jonathan.e.hart@outlook.com>
2025-12-24 12:20:54 -0500

    Added waybar to nix config

--- nixos/modules/home/desktop.nix
+++ nixos/modules/home/desktop.nix
@@ -4,6 +4,12 @@
   xdg.configFile."hypr/hyprland.conf".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/.config/hypr/hyprland.conf";
 
+  # TODO: Switch from waybar to AGS
+  programs.waybar.enable = true;
+
+  xdg.configFile."waybar".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/.config/waybar";
+
   programs.alacritty = {
     enable = true;
     settings = {
