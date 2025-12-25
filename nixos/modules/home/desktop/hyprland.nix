5e00b4427b1702889cf49491add7f9e542e25ed9
Jonathan Hart <jonathan.e.hart@outlook.com>
2025-12-25 17:04:25 -0500

    Added wl-clipboard to Nix

--- nixos/modules/home/desktop/hyprland.nix
+++ nixos/modules/home/desktop/hyprland.nix
@@ -1,6 +1,8 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   xdg.configFile."hypr/hyprland.conf".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/.config/hypr/hyprland.conf";
+
+  home.packages = with pkgs; [ wl-clipboard ];
 }
