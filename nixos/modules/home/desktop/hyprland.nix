6605ae5a67344a8adb9ee3f3c1e233c1a9e4b83d
Jonathan Hart <jonathan.e.hart@outlook.com>
2025-12-27 17:00:10 -0500

    Added polkit stuff

--- nixos/modules/home/desktop/hyprland.nix
+++ nixos/modules/home/desktop/hyprland.nix
@@ -4,5 +4,7 @@
   xdg.configFile."hypr/hyprland.conf".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/hypr/hyprland.conf";
 
+  services.hyprpolkitagent.enable = true;
+
   home.packages = with pkgs; [ wl-clipboard ];
 }
--- nixos/modules/system/common.nix
+++ nixos/modules/system/common.nix
@@ -31,4 +31,18 @@
     enable = true;
     enableSSHSupport = true;
   };
+
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (
+        (action.id == "org.freedesktop.login1.reboot" ||
+         action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+         action.id == "org.freedesktop.login1.power-off" ||
+         action.id == "org.freedesktop.login1.power-off-multiple-sessions") &&
+        subject.isInGroup("wheel")
+      ) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
 }
