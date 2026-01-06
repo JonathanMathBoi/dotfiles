1594703ee4ace908d0954697463753251f4af449
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-01-06 16:54:48 -0500

    Moved hyprland uwsm fix to system level

--- nixos/modules/system/hyprland.nix
+++ nixos/modules/system/hyprland.nix
@@ -5,4 +5,12 @@
     enable = true;
     withUWSM = true;
   };
+
+  # Ensure that all graphical apps close before hyprland closes
+  # Needed to ensure Brave (and other chromium based apps) close cleanly at DE teardown
+  systemd.user.services."wayland-wm@hyprland-uwsm.desktop" = {
+    unitConfig = {
+      Before = [ "graphical-session.target" ];
+    };
+  };
 }
