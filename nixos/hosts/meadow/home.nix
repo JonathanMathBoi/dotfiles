5aea7d67406ce0e5e85799fe7381b0f2732d1451
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-01-20 14:17:07 -0500

    Added meadow home manager config

---
+++ nixos/hosts/meadow/home.nix
@@ -0,0 +1,19 @@
+{ ... }:
+
+{
+  home.stateVersion = "25.11";
+  programs.home-manager.enable = true;
+
+  imports = [
+    ../../modules/home/common.nix
+    ../../modules/home/user-dirs.nix
+    ../../modules/home/desktop/common.nix
+    ../../modules/home/music/common.nix
+  ];
+
+  programs.git.signing.key = "E44941267E6C7C82";
+
+  xdg.configFile."hypr/monitors.conf".text = ''
+    monitor=eDP-1, 1920x1280@60, 0x0, 1.25
+  '';
+}
