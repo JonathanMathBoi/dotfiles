4fe9e11b330e9742b6aca9cde68c54702c7ea3c3
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-01-20 13:34:50 -0500

    Added disko config for meadow

---
+++ nixos/hosts/meadow/disko.nix
@@ -0,0 +1,62 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Override existing partition
+                subvolumes = {
+                  "@" = {
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                    mountpoint = "/";
+                  };
+                  "@home" = {
+                    mountOptions = [ "compress=zstd" ];
+                    mountpoint = "/home";
+                  };
+                  "@nix" = {
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                    mountpoint = "/nix";
+                  };
+                  "@swap" = {
+                    mountOptions = [
+                      "noatime"
+                      "nodatacow"
+                    ];
+                    mountpoint = "/swap";
+                    swap.swapfile.size = "12G";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
