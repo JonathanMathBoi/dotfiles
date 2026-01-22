8d8e9a5c3302f963d40dbde93ccc5337e4835e16
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-01-21 23:04:06 -0500

    Added NIXOS_OZONE_WL=1 to hyprlauncher prefix

--- nixos/modules/home/desktop/discord.nix
+++ nixos/modules/home/desktop/discord.nix
@@ -2,18 +2,4 @@
 
 {
   programs.discord.enable = true;
-
-  xdg.desktopEntries.discord = {
-    name = "Discord";
-    # Force Discord to run natively on Wayland
-    exec = "discord --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform=wayland";
-    icon = "discord";
-    genericName = "All-in-one voice and text chat";
-    categories = [
-      "Network"
-      "InstantMessaging"
-    ];
-    terminal = false;
-    mimeType = [ "x-scheme-handler/discord" ];
-  };
 }
--- nixos/modules/home/desktop/hyprlauncher.nix
+++ nixos/modules/home/desktop/hyprlauncher.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = {
       finders = {
-        desktop_launch_prefix = "uwsm app -- ";
+        desktop_launch_prefix = "env NIXOS_OZONE_WL=1 uwsm app -- ";
       };
     };
   };
