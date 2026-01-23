e1b27c1639993533162d8e5160bd82ff2d80e686
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-01-23 14:26:32 -0500

    Added wallpaper to meadow hyprlock

--- nixos/hosts/meadow/home.nix
+++ nixos/hosts/meadow/home.nix
@@ -26,4 +26,20 @@
       }
     ];
   };
+
+  programs.hyprlock.settings = {
+    background = [
+      {
+        monitor = "eDP-1";
+        path = "~/dotfiles/wallpapers/longwood_gardens_nov_2025_1920x1200.jpg";
+        blur_size = 4;
+        blur_passes = 3;
+        noise = 0.0117;
+        contrast = 1.3;
+        brightness = 0.8;
+        vibrancy = 0.21;
+        vibrancy_darkness = 0.0;
+      }
+    ];
+  };
 }
