6ca0f1baa5ae159a65d61307f9375eb2ab91f882
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-01-23 14:22:08 -0500

    Added wallpaper to meadow

--- nixos/hosts/meadow/home.nix
+++ nixos/hosts/meadow/home.nix
@@ -16,4 +16,14 @@
   xdg.configFile."hypr/monitors.conf".text = ''
     monitor=eDP-1, 1920x1280@60, 0x0, 1.25
   '';
+
+  services.hyprpaper.settings = {
+    preload = [ "~/dotfiles/wallpapers/longwood_gardens_nov_2025_1920x1200.jpg" ];
+    wallpaper = [
+      {
+        monitor = "eDP-1";
+        path = "~/dotfiles/wallpapers/longwood_gardens_nov_2025_1920x1200.jpg";
+      }
+    ];
+  };
 }
