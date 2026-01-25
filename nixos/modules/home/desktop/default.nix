84a5eb592c3b130e9978ea3589f65962065422db
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-01-25 16:55:52 -0500

    Moved terminal options into terminals/default.nix

--- nixos/modules/home/desktop/default.nix
+++ nixos/modules/home/desktop/default.nix
@@ -26,18 +26,6 @@ in
   options.desktop = {
     enable = mkEnableOption "desktop environment";
 
-    terminal = mkOption {
-      type = types.enum [
-        "alacritty"
-        "kitty"
-      ];
-      default = "alacritty";
-      description = "The main terminal emulator for the DE to use.";
-    };
-
-    alacritty.enable = mkEnableOption "alacritty";
-    kitty.enable = mkEnableOption "kitty";
-
     browser = mkOption {
       type = types.enum [ "brave" ];
       default = "brave";
@@ -46,20 +34,6 @@ in
   };
 
   config = mkIf cfg.enable {
-    desktop.alacritty.enable = mkDefault (cfg.terminal == "alacritty");
-    desktop.kitty.enable = mkDefault (cfg.terminal == "kitty");
-
-    assertions = [
-      {
-        assertion = (cfg.terminal == "alacritty" -> cfg.alacritty.enable);
-        message = "desktop.terminal is set to 'alacritty', but desktop.alacritty.enable is false.";
-      }
-      {
-        assertion = (cfg.terminal == "kitty" -> cfg.kitty.enable);
-        message = "desktop.terminal is set to 'kitty', but desktop.kitty.enable is false.";
-      }
-    ];
-
     # Help with some apps to run in wayland rather then XWayland
     systemd.user.sessionVariables = {
       NIXOS_OZONE_WL = "1";
--- nixos/modules/home/desktop/terminals/default.nix
+++ nixos/modules/home/desktop/terminals/default.nix
@@ -1,8 +1,42 @@
-{ ... }:
+{ lib, config, ... }:
 
+with lib;
+let
+  cfg = config.desktop;
+in
 {
   imports = [
     ./alacritty.nix
     ./kitty.nix
   ];
+
+  options.desktop = {
+    terminal = mkOption {
+      type = types.enum [
+        "alacritty"
+        "kitty"
+      ];
+      default = "alacritty";
+      description = "The main terminal emulator for the DE to use.";
+    };
+
+    alacritty.enable = mkEnableOption "alacritty";
+    kitty.enable = mkEnableOption "kitty";
+  };
+
+  config = mkIf cfg.enable {
+    desktop.alacritty.enable = mkDefault (cfg.terminal == "alacritty");
+    desktop.kitty.enable = mkDefault (cfg.terminal == "kitty");
+
+    assertions = [
+      {
+        assertion = (cfg.terminal == "alacritty" -> cfg.alacritty.enable);
+        message = "desktop.terminal is set to 'alacritty', but desktop.alacritty.enable is false.";
+      }
+      {
+        assertion = (cfg.terminal == "kitty" -> cfg.kitty.enable);
+        message = "desktop.terminal is set to 'kitty', but desktop.kitty.enable is false.";
+      }
+    ];
+  };
 }
