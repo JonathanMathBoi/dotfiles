626f7112cddd326e41eb6d326d3b727441dbf95e
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-01-30 11:32:12 -0500

    Added events to todo-comments lazy loading

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -2,6 +2,7 @@ return {
   'folke/todo-comments.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
   opts = {},
+  event = { 'BufReadPost', 'BufNewFile' },
   keys = {
     { '<leader>ft', '<CMD>TodoTelescope<CR>', desc = 'Find Todos' },
   },
