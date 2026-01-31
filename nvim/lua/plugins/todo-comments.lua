0d95f42ca0c1b5814b8fe1b07dd4ec53873bf4a5
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-01-30 19:40:42 -0500

    Changed todo-comments to VeryLazy loading

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -2,7 +2,7 @@ return {
   'folke/todo-comments.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
   opts = {},
-  event = { 'BufReadPost', 'BufNewFile' },
+  event = 'VeryLazy',
   keys = {
     { '<leader>ft', '<CMD>TodoTelescope<CR>', desc = 'Find Todos' },
   },
