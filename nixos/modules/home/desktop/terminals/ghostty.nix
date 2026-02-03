4d7ab40adfa21fb31621744309ef560e3ad141b4
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-02-03 15:51:07 -0500

    Added some extra config to ghostty

--- nixos/modules/home/desktop/terminals/ghostty.nix
+++ nixos/modules/home/desktop/terminals/ghostty.nix
@@ -9,6 +9,18 @@ in
     programs.ghostty = {
       enable = true;
       enableFishIntegration = true;
+
+      settings = {
+        font-family = "JetBrainsMono Nerd Font";
+        font-size = 11.25;
+
+        background-opacity = 0.82;
+        alpha-blending = "native";
+
+        # Performance & Integration (recommended for your stack)
+        # window-decoration = false; # Clean look like Alacritty
+        # confirm-close-surface = false; # Don't prompt when closing via tmux
+      };
     };
   };
 }
