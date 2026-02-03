faca85fee88bc6a441b99eec6a83844e72c1052d
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-02-03 16:45:42 -0500

    Updated ghostty to use alacrity default colors

--- nixos/modules/home/desktop/terminals/ghostty.nix
+++ nixos/modules/home/desktop/terminals/ghostty.nix
@@ -19,6 +19,32 @@ in
 
         window-decoration = false;
         confirm-close-surface = false;
+
+        theme = "alacritty-default";
+      };
+
+      themes.alacritty-default = {
+        background = "181818";
+        foreground = "d8d8d8";
+
+        palette = [
+          "0=#181818"
+          "1=#ac4242"
+          "2=#90a959"
+          "3=#f4bf75"
+          "4=#6a9fb5"
+          "5=#aa759f"
+          "6=#75b5aa"
+          "7=#d8d8d8"
+          "8=#6b6b6b"
+          "9=#c55555"
+          "10=#aac474"
+          "11=#feca88"
+          "12=#82b8c8"
+          "13=#c28cb8"
+          "14=#93d3c3"
+          "15=#f8f8f8"
+        ];
       };
     };
 
