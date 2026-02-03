5b1d60fd5d0f51216b7ed99ccbbadb2e4bac64b2
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-02-03 17:00:10 -0500

    Added terminal passthrough to tmux

--- nixos/modules/home/tmux.nix
+++ nixos/modules/home/tmux.nix
@@ -15,6 +15,10 @@
     extraConfig = ''
       # Make sure full color works on all terminals
       set -as terminal-overrides ",*:Tc"
+      # Pass through terminal so programs can do things like images
+      set -g allow-passthrough on
+      set -ga update-environment TERM
+      set -ga update-environment TERM_PROGRAM
 
       # Renumber windows when one closes
       set -g renumber-windows on
