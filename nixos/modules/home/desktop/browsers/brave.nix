1ced24cf5c735b1efe410c192ecb3cb60d5fc376
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-02-03 19:16:37 -0500

    Added Brave scaling bug comment

--- nixos/modules/home/desktop/browsers/brave.nix
+++ nixos/modules/home/desktop/browsers/brave.nix
@@ -12,6 +12,7 @@ in
         # BUG: Surface Stylus crashes on Wayland when stylus and then touchpad
         # Submitt bug report and switch back to Wayland as soon as possible
         "--ozone-platform=x11"
+        # BUG: Make 1.25x scaling only apply to meadow
         "--force-device-scale-factor=1.25"
       ];
     };
