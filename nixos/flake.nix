9f067c118ec91ec84fe1683b1c643565df6a7848
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-02-14 12:14:09 -0500

    Reverted nixos-hardware to build for meadow

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -12,7 +12,8 @@
     };
     # BUG: Main NixOS Hardware branch's surface kernel is behind and can't compile
     # Switch to main nixos-hardware branch when PR is merged
-    nixos-hardware.url = "github:mexisme/nixos-hardware/microsoft-surface/update-kernel-6.18.8";
+    # nixos-hardware PR #1763
+    nixos-hardware.url = "github:8bitbuddhist/nixos-hardware/surface-kernel-6.18";
   };
   outputs =
     inputs@{
