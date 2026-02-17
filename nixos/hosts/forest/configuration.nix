a0d4ccc00abd6000b3ea8f036338669a9b0fedd2
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-02-17 14:46:03 -0500

    Corrected sshd impermanence setup

--- nixos/hosts/forest/configuration.nix
+++ nixos/hosts/forest/configuration.nix
@@ -95,11 +95,14 @@
       "/var/lib/systemd/coredump"
       "/var/lib/systemd/timers"
       "/etc/NetworkManager/system-connections"
-      "/etc/ssh"
       "/etc/secrets"
     ];
     files = [
       "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
     ];
   };
 
