d305a84b4f2c5ac3f4c909593625f45138cbf677
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-02-17 17:52:44 -0500

    Added TODO to fix impermanence

--- nixos/hosts/forest/configuration.nix
+++ nixos/hosts/forest/configuration.nix
@@ -107,6 +107,10 @@
     ];
   };
 
+  # FIXME: Bind mount for secrets so passwords work
+  # Remove when implementing sops-nix
+  # TODO: Add bind mount for /etc/secrets
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
