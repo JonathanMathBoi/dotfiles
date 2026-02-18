f685774ac60ac2c5d0e99aa0169705efc96a9971
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-02-18 14:01:41 -0500

    Moved forest impermanence config to separate file

---
+++ nixos/hosts/forest/persistence.nix
@@ -0,0 +1,36 @@
+{ ... }:
+
+{
+  environment.persistence."/persist" = {
+    enable = true;
+    hideMounts = true;
+    directories = [
+      "/var/log"
+      "/var/db/sudo"
+      "/var/lib/bluetooth"
+      "/var/lib/nixos"
+      "/var/lib/docker"
+      "/var/lib/tailscale"
+      "/var/lib/systemd/coredump"
+      "/var/lib/systemd/timers"
+      "/etc/NetworkManager/system-connections"
+      "/var/cache/tuigreet"
+    ];
+    files = [
+      "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+    ];
+  };
+
+  # HACK: Bind mount for secrets so passwords work
+  # Remove when implementing sops-nix
+  fileSystems."/etc/secrets" = {
+    device = "/persist/etc/secrets";
+    fsType = "none";
+    neededForBoot = true;
+    options = [ "bind" ];
+  };
+}
