3818c69d55b6ee155a970dfde764e94f1462393a
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-02-24 11:55:05 -0500

    Updated TODOs

--- hosts/forest/configuration.nix
+++ hosts/forest/configuration.nix
@@ -86,8 +86,6 @@
     };
   };
 
-  # TODO: Convert forest SSD to LUKS
-
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
