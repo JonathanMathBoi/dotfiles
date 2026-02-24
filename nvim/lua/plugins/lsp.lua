ba9062d409749d098c0240f16c8d4767172a2c29
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-02-24 12:53:57 -0500

    Simplified lsp.lua
    Removed extra braces since we don't use any other plugins besides
    nvim-lspconfig to manage LSPs.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,58 +1,56 @@
 return {
-  {
-    'neovim/nvim-lspconfig',
-    config = function()
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-        callback = function(ev)
-          -- Gotos
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Goto Definition', buffer = ev.buf })
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Goto Declaration', buffer = ev.buf })
-          vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, { desc = 'Goto Type Definition', buffer = ev.buf })
-          vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'Goto References', buffer = ev.buf })
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = 'Goto Implementation', buffer = ev.buf })
+  'neovim/nvim-lspconfig',
+  config = function()
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+      callback = function(ev)
+        -- Gotos
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Goto Definition', buffer = ev.buf })
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Goto Declaration', buffer = ev.buf })
+        vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, { desc = 'Goto Type Definition', buffer = ev.buf })
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'Goto References', buffer = ev.buf })
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = 'Goto Implementation', buffer = ev.buf })
 
-          -- Diagnostic Group (<leader>d)
-          vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float, { desc = 'Floating Diagnostic' })
-          vim.keymap.set('n', '<leader>dq', vim.diagnostic.setqflist, { desc = 'Diagnostics to Quickfix' })
+        -- Diagnostic Group (<leader>d)
+        vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float, { desc = 'Floating Diagnostic' })
+        vim.keymap.set('n', '<leader>dq', vim.diagnostic.setqflist, { desc = 'Diagnostics to Quickfix' })
 
-          -- Code Group (<leader>c)
-          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code Action', buffer = ev.buf })
-          vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, { desc = 'Rename Symbol', buffer = ev.buf })
-          vim.keymap.set('n', '<leader>cs', vim.lsp.buf.hover, { desc = 'Code Signature', buffer = ev.buf })
-        end,
-      })
+        -- Code Group (<leader>c)
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code Action', buffer = ev.buf })
+        vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, { desc = 'Rename Symbol', buffer = ev.buf })
+        vim.keymap.set('n', '<leader>cs', vim.lsp.buf.hover, { desc = 'Code Signature', buffer = ev.buf })
+      end,
+    })
 
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      vim.lsp.config('*', {
-        capabilities = capabilities,
-      })
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    vim.lsp.config('*', {
+      capabilities = capabilities,
+    })
 
-      -- Lua setup (keeps 'vim' global from complaining)
-      vim.lsp.config('lua_ls', {
-        settings = {
-          Lua = {
-            diagnostics = { globals = { 'vim' } },
-          },
+    -- Lua setup (keeps 'vim' global from complaining)
+    vim.lsp.config('lua_ls', {
+      settings = {
+        Lua = {
+          diagnostics = { globals = { 'vim' } },
         },
-      })
-      vim.lsp.enable('lua_ls')
+      },
+    })
+    vim.lsp.enable('lua_ls')
 
-      -- Nix LSP
-      vim.lsp.enable('nixd')
+    -- Nix LSP
+    vim.lsp.enable('nixd')
 
-      -- Rust LSP
-      vim.lsp.enable('rust_analyzer')
+    -- Rust LSP
+    vim.lsp.enable('rust_analyzer')
 
-      -- Enable web deb LSPs
-      vim.lsp.enable('ts_ls')
-      vim.lsp.enable('cssls')
-      vim.lsp.enable('jsonls')
-      vim.lsp.enable('html')
-      vim.lsp.enable('emmet_ls')
+    -- Enable web deb LSPs
+    vim.lsp.enable('ts_ls')
+    vim.lsp.enable('cssls')
+    vim.lsp.enable('jsonls')
+    vim.lsp.enable('html')
+    vim.lsp.enable('emmet_ls')
 
-      -- Enable Java LSP
-      vim.lsp.enable('jdtls')
-    end,
-  },
+    -- Enable Java LSP
+    vim.lsp.enable('jdtls')
+  end,
 }
