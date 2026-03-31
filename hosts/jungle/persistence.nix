f2a44d7e7ab11117a2ec9f725ac80fe6266279f1
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-03-30 23:20:04 -0400

    Added jungle disko and persistence foundation

---
+++ hosts/jungle/persistence.nix
@@ -0,0 +1,27 @@
+{ ... }:
+
+{
+  environment.persistence."/persist" = {
+    enable = true;
+    hideMounts = true;
+
+    directories = [
+      "/etc/luks-keys"
+      "/var/log"
+      "/var/db/sudo"
+      "/var/lib/nixos"
+      "/var/lib/tailscale"
+      "/var/lib/systemd/coredump"
+      "/var/lib/systemd/timers"
+      "/etc/NetworkManager/system-connections"
+    ];
+
+    files = [
+      "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+    ];
+  };
+}
