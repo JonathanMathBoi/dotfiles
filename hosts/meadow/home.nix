fe554936f6f340b57ec25045cbd774193c365adf
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-04-11 10:24:47 -0400

    Moved meadow display config into surface-go-3 home profile
    Extracted the Hyprland monitor, hyprpaper, and hyprlock settings into a Surface Go 3-specific Home Manager module and imported it from meadow home.nix to keep host-level config hardware-agnostic.

--- hosts/meadow/home.nix
+++ hosts/meadow/home.nix
@@ -6,6 +6,7 @@
 
   imports = [
     ../../modules/home
+    ./surface-go-3/home.nix
   ];
 
   dots = {
@@ -26,36 +27,6 @@
   };
 
   programs.git.signing.key = "E44941267E6C7C82";
-
-  wayland.windowManager.hyprland.settings.monitor = [
-    "eDP-1, 1920x1280@60, 0x0, 1.25"
-  ];
-
-  services.hyprpaper.settings = {
-    preload = [ "~/dotfiles/wallpapers/longwood_gardens_nov_2025_1920x1200.jpg" ];
-    wallpaper = [
-      {
-        monitor = "eDP-1";
-        path = "~/dotfiles/wallpapers/longwood_gardens_nov_2025_1920x1200.jpg";
-      }
-    ];
-  };
-
-  programs.hyprlock.settings = {
-    background = [
-      {
-        monitor = "eDP-1";
-        path = "~/dotfiles/wallpapers/longwood_gardens_nov_2025_1920x1200.jpg";
-        blur_size = 4;
-        blur_passes = 3;
-        noise = 0.0117;
-        contrast = 1.3;
-        brightness = 0.8;
-        vibrancy = 0.21;
-        vibrancy_darkness = 0.0;
-      }
-    ];
-  };
 
   # Makes sure MPD can get to music library
   systemd.user.services.mpd.Service.RequiresMountsFor = "/home/jonathan/music";
