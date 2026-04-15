1a7de41bba14e25a738b42c1e45de039d6563e89
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-04-15 11:42:21 -0400

    Updated meadow scaling for Framework 12

--- hosts/meadow/framework-12/home.nix
+++ hosts/meadow/framework-12/home.nix
@@ -2,7 +2,7 @@
 
 {
   wayland.windowManager.hyprland.settings.monitor = [
-    "eDP-1,1920x1200@60,0x0,1.1"
+    "eDP-1,1920x1200@60,0x0,1.07"
   ];
 
   services.hyprpaper.settings = {
--- modules/home/desktop/browsers/brave.nix
+++ modules/home/desktop/browsers/brave.nix
@@ -18,8 +18,8 @@ in
         # Submitt bug report and switch back to Wayland as soon as possible
         "--ozone-platform=x11"
       ]
-      # HACK: Make Brave use 1.25 scale on meadow because of XWayland being needed for stylus crash prevention
-      ++ optional (osConfig.networking.hostName == "meadow") "--force-device-scale-factor=1.25";
+      # HACK: Make Brave use 1.07 scale on meadow because of XWayland being needed for stylus crash prevention
+      ++ optional (osConfig.networking.hostName == "meadow") "--force-device-scale-factor=1.07";
     };
   };
 }
