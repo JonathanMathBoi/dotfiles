e816ded84e1c793984aa043e0a436a5882114b64
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-06-03 09:38:35 -0400

    Updated Neovim bufferline to use Catppuccin

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,6 +1,9 @@
 return {
   'akinsho/bufferline.nvim',
   dependencies = 'nvim-tree/nvim-web-devicons',
-  opts = {},
-  -- TODO: Update to use Catppuccin theme
+  opts = function(_, opts)
+    if (vim.g.colors_name or ''):find('catppuccin') then
+      opts.highlights = require('catppuccin.special.bufferline').get_theme()
+    end
+  end,
 }
