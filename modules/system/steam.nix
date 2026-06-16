7f8f1873beae1cf840b97cde600dd72a36730c86
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-06-15 20:47:26 -0400

    Fixed Steam Controller not visually moving cursor

--- modules/system/steam.nix
+++ modules/system/steam.nix
@@ -7,6 +7,9 @@
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
     gamescopeSession.enable = true;
+
+    # Enabled so that Steam Controller cursor handeling works right
+    extest.enable = true;
   };
 
   programs.gamescope.enable = true;
