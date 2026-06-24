a9421f06ba01a5880e2cb5c9b957155b74c402a0
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-06-24 12:21:58 -0400

    Added Window management keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,11 +3,19 @@ vim.g.mapleader = ' '
 -- TODO: Learn what localleader is
 vim.g.maplocalleader = '\\'
 
--- Set <C-direction> to move between splits
-vim.keymap.set({ 'n', 't' }, '<C-h>', '<C-w>h')
-vim.keymap.set({ 'n', 't' }, '<C-j>', '<C-w>j')
-vim.keymap.set({ 'n', 't' }, '<C-k>', '<C-w>k')
-vim.keymap.set({ 'n', 't' }, '<C-l>', '<C-w>l')
+-- Window Commands
 
--- Buffer Close
-vim.keymap.set({ 'n', 't' }, '<leader>bd', '<CMD>:bd<CR>', { desc = 'Delete Buffer and Window' })
+-- NOTE: See nvim-tmux-nav plugin for window nav keymaps
+-- Set <C-direction> to move between splits
+-- vim.keymap.set({ 'n', 't' }, '<C-h>', '<C-w>h')
+-- vim.keymap.set({ 'n', 't' }, '<C-j>', '<C-w>j')
+-- vim.keymap.set({ 'n', 't' }, '<C-k>', '<C-w>k')
+-- vim.keymap.set({ 'n', 't' }, '<C-l>', '<C-w>l')
+
+-- Window Management
+vim.keymap.set('n', '<leader>wv', '<CMD>vsplit<CR>', { desc = 'Split window vertically' })
+vim.keymap.set('n', '<leader>wh', '<CMD>split<CR>', { desc = 'Split Window Horizontally' })
+vim.keymap.set('n', '<leader>wd', '<CMD>close<CR>', { desc = 'Delete Window' })
+
+-- Buffer Commands
+vim.keymap.set({ 'n', 't' }, '<leader>bd', '<CMD>bd<CR>', { desc = 'Delete Buffer and Window' })
