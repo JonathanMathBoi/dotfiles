8b5ed097149a2aff3efac5cab94a8e4ce50b1513
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-06-24 14:53:24 -0400

    Simplified OSC 52 nvim setup

--- nvim/lua/config/clipboard.lua
+++ nvim/lua/config/clipboard.lua
@@ -10,16 +10,5 @@ if in_tmux then
   vim.g.clipboard = 'tmux'
 elseif in_ssh then
   -- Use OSC 52 over SSH when not in tmux
-  local osc52 = require('vim.ui.clipboard.osc52')
-  vim.g.clipboard = {
-    name = 'OSC 52',
-    copy = {
-      ['+'] = osc52.copy('+'),
-      ['*'] = osc52.copy('*'),
-    },
-    paste = {
-      ['+'] = osc52.paste('+'),
-      ['*'] = osc52.paste('*'),
-    },
-  }
+  vim.g.clipboard = 'osc52'
 end
