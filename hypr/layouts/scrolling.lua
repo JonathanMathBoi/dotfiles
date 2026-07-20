228408f077bc320d8acbbc5069631539ea5d8d36
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-07-20 12:04:04 -0400

    Added basic scrolling config and bindings

--- hypr/layouts/scrolling.lua
+++ hypr/layouts/scrolling.lua
@@ -1,6 +1,15 @@
+-- General Config
 hl.config({
   general = { layout = 'scrolling' },
   scrolling = {
     direction = 'right',
+    column_width = 0.8,
+    widths = { 0.5, 0.8, 1.0 },
   },
 })
+
+-- Binding Configurations
+-- TODO: Fine a way to unify with binding configs
+local main_mod = 'SUPER'
+
+hl.bind(main_mod .. ' + T', hl.dsp.layout('colresize +conf'))
