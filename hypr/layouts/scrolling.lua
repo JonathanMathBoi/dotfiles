3b3233615eb483140dc4c4b4c7f471bb3687563e
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-07-20 14:38:53 -0400

    Added column movement bindings to scrolling

--- hypr/layouts/scrolling.lua
+++ hypr/layouts/scrolling.lua
@@ -12,6 +12,16 @@ hl.config({
 -- TODO: Fine a way to unify with binding configs
 local main_mod = 'SUPER'
 
+-- Swap columns left and right
+hl.bind(main_mod .. ' + bracketleft', hl.dsp.layout('swapcol l'))
+hl.bind(main_mod .. ' + bracketright', hl.dsp.layout('swapcol r'))
+
+-- Compress and expand behavior
+-- If window is alone in column, compress into
+-- If window is not alone, expand out of
+hl.bind(main_mod .. ' + COMMA', hl.dsp.layout('consume_or_expel next'))
+
+-- Cycle column widths
 hl.bind(main_mod .. ' + T', hl.dsp.layout('colresize +conf'))
 
 -- Gestures Configuration
