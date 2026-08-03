312e618ff320964557838d3173e6b08499cdf9c6
Jonathan Hart <jonathan.e.hart@outlook.com>
2026-08-03 15:18:13 -0400

    Added Discord window rule

--- hypr/window_rules.lua
+++ hypr/window_rules.lua
@@ -1,6 +1,13 @@
 hl.window_rule({
   name = 'Xournal++ Scroll Width',
-  match = { class = 'com.github.xournalpp.xournalpp' },
   -- Launch Xournal windows with full screen width on scrolling layout
+  match = { class = 'com.github.xournalpp.xournalpp' },
+  scrolling_width = 1.0,
+})
+
+hl.window_rule({
+  name = 'Discord Scroll Width',
+  -- Launch Discord with full screen width on scrolling layout
+  match = { class = 'discord' },
   scrolling_width = 1.0,
 })
